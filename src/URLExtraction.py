e5550dd1f3ceecf5aa47afe2cb6b24f366b2bc26
fkayode <36118265+fkayode@users.noreply.github.com>
2020-08-09 10:12:24 -0400

    Extracted from URL

---
+++ src/URLExtraction.py
@@ -0,0 +1,80 @@
+
+import sys
+
+from dotenv import load_dotenv, find_dotenv
+import os
+import json
+import itertools
+import pandas as pd
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import (
+    Features,
+    CategoriesOptions,
+    KeywordsOptions,
+    ConceptsOptions,
+)
+
+# load the .env file containing your environment variables for the required
+load_dotenv(find_dotenv())
+
+"""
+Initialize NLU Instance with Environment Varibles Stored in .env
+"""
+
+authenticator = IAMAuthenticator(os.environ.get('NATURAL_LANGUAGE_UNDERSTANDING_APIKEY'))
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version="2019-07-12", authenticator=authenticator
+)
+
+natural_language_understanding.set_service_url(os.environ.get("NATURAL_LANGUAGE_UNDERSTANDING_URL"))
+
+"""
+INPUT: filepath to an input csv file 
+OUTPUT: csv file that contains courses, descriptions, keywords, concepts and subjects
+Notes:
+- Make sure that your input file mirrors the formatting of the input file in 
+./data/discovery-nlu/input/ElementarySchoolClasses.csv
+"""
+def extractEntities(input_filepath, output_filepath):
+    df = pd.read_csv(input_filepath)
+    (rows, _) = df.shape
+    for idx in range(0, rows, 1):
+        hotline_url = df["URL"][idx]
+        nlu_categories = natural_language_understanding.analyze(
+            url=hotline_url, features=Features(categories=CategoriesOptions())
+        ).get_result()
+        nlu_keywords = natural_language_understanding.analyze(
+            url=hotline_url,
+            features=Features(keywords=KeywordsOptions(sentiment=True, emotion=True)),
+        ).get_result()
+        nlu_concepts = natural_language_understanding.analyze(
+            url=hotline_url, features=Features(concepts=ConceptsOptions())
+        ).get_result()
+        categories_list = list(map(lambda x: x["label"], nlu_categories["categories"]))
+        keywords_list = list(map(lambda x: x["text"], nlu_keywords["keywords"]))
+        concepts_list = list(map(lambda x: x["text"], nlu_concepts["concepts"]))
+        categories_list_extracted = list(
+            map(lambda x: x.split("/")[1:], categories_list)
+        )
+        categories_list_flattened = list(
+            set(list(itertools.chain(*categories_list_extracted)))
+        )
+        # If there are not enough concepts add keywords to the list
+        if len(concepts_list) < 3:
+            concepts_list = concepts_list + keywords_list
+        df["Concepts"][idx] = concepts_list
+        df["Subject"][idx] = categories_list_flattened
+    df.to_csv(output_filepath, index = False)
+    return df
+    
+
+
+# Sample Function Calls
+extractEntities(
+"./data/discovery-nlu/input/Hotlines.csv",
+"./data/discovery-nlu/output/Hotlines_Analyzed.csv"
+)
+
+
